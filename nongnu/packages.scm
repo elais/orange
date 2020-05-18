52e82b59ee7083613556e9e35f453e188b8fb502
Elais Player <elais@fastmail.com>
2020-05-17 18:09:12 -0600

    hide package manifests for now

--- nongnu/packages.scm
+++ nongnu/packages.scm
@@ -1,6 +1,6 @@
-(define-module (nongnu packages)
-  #:use-module (nongnu packages linux)
-  #:use-module (guix packages))
+;; (define-module (nongnu packages)
+;;   #:use-module (nongnu packages linux)
+;;   #:use-module (guix packages))
 
-(packages->manifest
- (list linux-5.4 linux-firmware))
+;; (packages->manifest
+;;  (list linux-5.4 linux-firmware))
